4574e5f13619eb97c23d68f4581f716c59bdfa3c
Shin Nakamura <sn@i.basicwerk.com>
2018-12-07 08:41:05 +0900

    main -> run

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 extern crate csv_convert_job_rs;
-
 use csv_convert_job_rs::arg::get_nth_arg;
 
-fn main() {
-    let first_arg = get_nth_arg::get(1).unwrap();
+use std::error::Error;
+use std::process;
+
+fn run() -> Result<(), Box<Error>> {
+    let first_arg = get_nth_arg::get(1)?;
     println!("first arg is {:?}", first_arg);
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
 }
