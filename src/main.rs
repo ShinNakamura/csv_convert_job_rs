ee25e43a4ac52bde48905a5ee7953f75f48b2dec
Shin Nakamura <sn@i.basicwerk.com>
2018-12-07 09:07:12 +0900

    samples/config.csv deserialize -> Record

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 extern crate csv_convert_job_rs;
 use csv_convert_job_rs::arg::get_nth_arg;
 
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
 use std::error::Error;
 use std::process;
 
+#[derive(Debug, Deserialize)]
+struct Record {
+    row: usize,
+    column: usize,
+    name: String,
+}
+
 fn run() -> Result<(), Box<Error>> {
-    let first_arg = get_nth_arg::get(1)?;
-    println!("first arg is {:?}", first_arg);
+    let config_csv_path = get_nth_arg::get(1)?;
+    let mut rdr = csv::Reader::from_path(config_csv_path)?;
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        println!("{:?}", record);
+    }
 
     Ok(())
 }
